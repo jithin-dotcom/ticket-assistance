e974e650acd2f0514df3ee47838419ef4f7506a4
jithin-dotcom <jithinp2906@gmail.com>
2025-11-09 22:51:13 +0530

    feat(email): email send using inngest pipeline

---
+++ server/inngest/functions/onSignUp.js
@@ -0,0 +1,35 @@
+import { inngest } from "../client";
+import User from "../../models/user.js";
+import {NonRetriableError} from "inngest"
+import { sendMail } from "../../utils/mailer.js";
+
+export const onUserSignup = inngest.createFunction(
+     { id: "on-user-signup", retries: 2 },
+     { event: "user/signup" },
+     async ({event, step}) =>{
+        try {
+            const {email} = event.email;
+            const user = await step.run("get-user-email", async()=>{
+                const userObject = await User.findOne({email});
+                if(!userObject){
+                    throw new NonRetriableError("user no longer exist in our Db");
+                }
+                return userObject;
+            })
+            await step.run("send-welcome-email", async () => {
+                const subject = `welcome to the app`;
+                const message = `Hi,
+                \n\n
+                Thanks for signing up, we are glad to have you!
+                `
+                await sendMail(user.email, subject, message);
+            })
+
+            return {success: true};
+        } catch (error) {
+            console.error(" Error in running step : ",error.message);
+            return {success: false};
+        }
+     }
+
+)
